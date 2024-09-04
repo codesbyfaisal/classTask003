0281673ca2a3e5523eb1677b8eab2f4eb65db4de
codebyfaisal <fm0320939695@gmail.com>
2024-09-04 09:37:18 +0500

    create quiz questions using chatgpt

---
+++ script.js
@@ -0,0 +1,27 @@
+const quiz = [
+  {
+    question: "Which HTML tag is used to define an ordered list?",
+    options: ["<ul>", "<ol>", "<li>", "<list>", "<item>"],
+    answer: "<ol>"
+  },
+  {
+    question: "Which CSS property is used to change the background color of an element?",
+    options: ["font-size", "color", "text-align", "background-color", "border-color"],
+    answer: "background-color"
+  },
+  {
+    question: "What does 'HTML' stand for?",
+    options: ["HyperText Markup Language", "HyperText Modeling Language", "Hyperlink Text Markup Language", "HyperText Multilayer Language", "HyperText Multicolored Language"],
+    answer: "HyperText Markup Language"
+  },
+  {
+    question: "Which method is used to select an element by its ID in JavaScript?",
+    options: ["getElementById", "querySelector", "getElementByClassName", "getElementsByTagName", "selectById"],
+    answer: "getElementById"
+  },
+  {
+    question: "Which HTML attribute is used to specify an alternative text for an image?",
+    options: ["alt", "src", "title", "href", "class"],
+    answer: "alt"
+  }
+];
